39148d6707b108fbf2d8dbb362946664708411ea
Lukas Matt <lukas@zauberstuhl.de>
2018-03-13 18:50:54 +0100

    Revert "kill fed"
    This reverts commit cede964f40a2c7af59a2f17e9cf800a34470179e.

--- signature.go
+++ signature.go
@@ -64,8 +64,6 @@ func (signature *Signature) Sign(privKey *rsa.PrivateKey, sig *string) error {
 }
 
 func (signature *Signature) Verify(order string, pubKey *rsa.PublicKey) bool {
-  return false
-
   sig, err := base64.URLEncoding.DecodeString(signature.entity.Signature())
   if err != nil {
     sig, err = base64.StdEncoding.DecodeString(signature.entity.Signature())
